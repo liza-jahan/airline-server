e138471dd4c62dadada095a3692d4ac296d02d98
Eakteker jahan Liza <eaktekerjahan@gmail.com>
2024-09-27 15:07:22 +0600

    initial project.

---
+++ src/main/java/com/lina/airline/AirlineServerApplication.java
@@ -0,0 +1,13 @@
+package com.lina.airline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlineServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lina/airline/AirlineServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lina.airline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
