991177979a907c022d73d8cc6e393ebc8f295f5e
Eakteker jahan Liza <eaktekerjahan@gmail.com>
2024-09-29 23:27:17 +0600

    update UserEntity class

--- src/main/java/com/lina/airline/service/UserService.java
+++ src/main/java/com/lina/airline/service/UserService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
-@Service
+
 public interface  UserService {
 
     UUID saveUser(UserRegistrationRequest userRegistrationRequest);
--- src/main/java/com/lina/airline/utils/ValidUtils.java
+++
@@ -1,6 +0,0 @@
-package com.lina.airline.utils;
-
-public class ValidUtils {
-    public static final String nameRegex="^[a-zA-Z\\s\\-\\']*$";
-    public static final  String passwordRegex="^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]+$";
-}
