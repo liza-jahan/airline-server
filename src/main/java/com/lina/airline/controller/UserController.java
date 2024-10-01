03f772159b510a5d310002be8587cc6f39a55dee
Eakteker jahan Liza <eaktekerjahan@gmail.com>
2024-10-02 02:50:34 +0600

    Create CreationResponse class and update UserController

--- src/main/java/com/lina/airline/controller/UserController.java
+++ src/main/java/com/lina/airline/controller/UserController.java
@@ -1,14 +1,19 @@
 package com.lina.airline.controller;
 
 
-import com.lina.airline.dto.request.UpdateUserRegistrationRequest;
+import com.lina.airline.dto.APIResponse;
+import com.lina.airline.dto.request.UpdateUserRequest;
 import com.lina.airline.dto.request.UserRegistrationRequest;
+import com.lina.airline.dto.response.CreationResponse;
 import com.lina.airline.entity.UserEntity;
 import com.lina.airline.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -24,9 +29,18 @@ public class UserController {
         return userService.saveUser(userRegistrationRequest);
     }
   @PutMapping ("/updateUser/{id}")
-    public UUID updateUserRegistration(@RequestBody @Valid UpdateUserRegistrationRequest updateUserRegistrationRequest, @PathVariable UUID id){
-    Optional<UserEntity> userEntity=userService.updateUser(id,updateUserRegistrationRequest);
-      return userEntity.get().getId();
+    public ResponseEntity<APIResponse<CreationResponse>> updateUserProfile(@RequestBody @Valid UpdateUserRequest updateUserRegistrationRequest, @PathVariable UUID id){
+    Optional<UserEntity> userEntity=userService.updateUserProfile(id,updateUserRegistrationRequest);
+      APIResponse<CreationResponse> responseDTO = APIResponse
+              .<CreationResponse>builder()
+              .dateTime(new Date().toString())
+              .status(HttpStatus.OK.getReasonPhrase())
+              .code(HttpStatus.OK)
+              .results(new CreationResponse(userEntity.get().getId()))
+              .build();
+
+      return new ResponseEntity<>(responseDTO, HttpStatus.OK);
+
     }
 }
 
