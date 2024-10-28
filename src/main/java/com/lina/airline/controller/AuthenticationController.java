e03b51fe77893e89d307f1a06e1b736347716814
Eakteker jahan Liza <eaktekerjahan@gmail.com>
2024-10-28 21:21:09 +0600

    Update this issue.Moved logic from AuthenticationController to AuthenticationServiceImp.

--- src/main/java/com/lina/airline/controller/AuthenticationController.java
+++ src/main/java/com/lina/airline/controller/AuthenticationController.java
@@ -2,54 +2,25 @@ package com.lina.airline.controller;
 
 import com.lina.airline.dto.request.AuthenticationRequest;
 import com.lina.airline.dto.response.AuthenticationResponse;
+import com.lina.airline.service.AuthenticationService;
 import com.lina.airline.utils.ApplicationConstantsUtils;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.Keys;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.crypto.SecretKey;
-import java.nio.charset.StandardCharsets;
-import java.util.Date;
-import java.util.stream.Collectors;
-
 @RestController
-@RequiredArgsConstructor
-
+@AllArgsConstructor
 public class AuthenticationController {
-    private final AuthenticationManager authenticationManager;
-    @Value(ApplicationConstantsUtils.JWT_SECRET_DEFAULT_VALUE)
-    private String jwtSecretValue;
+    private AuthenticationService authenticationService;
 
     @PostMapping("/login")
     public ResponseEntity<AuthenticationResponse> login(@RequestBody AuthenticationRequest authRequest) {
-
-        String jwt = "";
-        Authentication authentication = UsernamePasswordAuthenticationToken.unauthenticated(authRequest.email(),
-                authRequest.password());
-        Authentication authenticationResponse = authenticationManager.authenticate(authentication);
-        if (authenticationResponse != null && authenticationResponse.isAuthenticated()) {
-            SecretKey secretKey = Keys.hmacShaKeyFor(jwtSecretValue.getBytes(StandardCharsets.UTF_8));
-            jwt = Jwts.builder()
-                    .setIssuer("Airline Server")
-                    .setSubject("JWT token")
-                    .claim("username", authentication.getName())
-                    .claim("authorities", authentication.getAuthorities().stream().map(
-                            GrantedAuthority::getAuthority).collect(Collectors.joining(",")))
-                    .setIssuedAt(new Date())
-                    .setExpiration(new Date((new Date()).getTime() + 30000000))
-                    .signWith(secretKey).compact();
-        }
-        return ResponseEntity.status(HttpStatus.OK).header(ApplicationConstantsUtils.JWT_HEADER, jwt).body(new AuthenticationResponse(HttpStatus.OK.getReasonPhrase(), jwt));
+        String userEntity = authenticationService.loginUser(authRequest);
+        return ResponseEntity.status(HttpStatus.OK).header(ApplicationConstantsUtils.JWT_HEADER, userEntity)
+                .body(new AuthenticationResponse(HttpStatus.OK.getReasonPhrase(), userEntity));
     }
 
 }
