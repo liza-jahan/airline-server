dad6246dd770415c63e6807ba89cae0c0838736f
Eakteker jahan Liza <eaktekerjahan@gmail.com>
2024-11-18 23:36:40 +0600

    Update AirCraftRepository and create IllegalException

--- src/main/java/com/lina/airline/airCraftSeatBooking/SeatsService.java
+++ src/main/java/com/lina/airline/airCraftSeatBooking/SeatsService.java
@@ -4,5 +4,5 @@ import java.util.List;
 import java.util.UUID;
 
 public interface SeatsService {
-    void createSeatsForAircraft (UUID aircraftId, int row, int colum);
+    void createSeatsForAircraft (UUID aircraftId, int row, int column);
 }
